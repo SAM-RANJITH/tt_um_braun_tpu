1664fd0fcd163874256a89fe04c387d583cf1671
SAM-RANJITH <141648923+SAM-RANJITH@users.noreply.github.com>
2026-03-12 15:50:40 +0530

    Create systolic_array_2x2.v

---
+++ src/systolic_array_2x2.v
@@ -0,0 +1,27 @@
+`default_nettype none
+
+module systolic_array_2x2(
+
+input wire clk,
+input wire rst,
+
+input wire [7:0] a0,
+input wire [7:0] a1,
+input wire [7:0] b0,
+input wire [7:0] b1,
+
+output wire [15:0] c00,
+output wire [15:0] c01,
+output wire [15:0] c10,
+output wire [15:0] c11
+);
+
+wire [7:0] a00,a01,a10,a11;
+wire [7:0] b00,b01,b10,b11;
+
+pe_braun pe00(clk,rst,a0,b0,a00,b00,c00);
+pe_braun pe01(clk,rst,a00,b1,a01,b01,c01);
+pe_braun pe10(clk,rst,a1,b00,a10,b10,c10);
+pe_braun pe11(clk,rst,a10,b01,a11,b11,c11);
+
+endmodule
