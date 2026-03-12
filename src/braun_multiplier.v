6ca807ec958edf681ea16bd07727b7b7081dca0c
SAM-RANJITH <141648923+SAM-RANJITH@users.noreply.github.com>
2026-03-12 16:35:43 +0530

    Update braun_multiplier.v

--- src/braun_multiplier.v
+++ src/braun_multiplier.v
@@ -8,18 +8,6 @@ output wire [15:0] P
 
 );
 
-wire [7:0] pp [7:0];
-
-genvar i,j;
-
-generate
-for(i=0;i<8;i=i+1) begin
-    for(j=0;j<8;j=j+1) begin
-        assign pp[i][j] = A[j] & B[i];
-    end
-end
-endgenerate
-
 assign P = A * B;
 
 endmodule
