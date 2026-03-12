eb4f24092dac37d53a24f9e6bde09ad5096ca9c8
SAM-RANJITH <141648923+SAM-RANJITH@users.noreply.github.com>
2026-03-12 15:50:19 +0530

    Create memory.v

---
+++ src/memory.v
@@ -0,0 +1,27 @@
+`default_nettype none
+
+module memory(
+    input wire clk,
+    input wire [7:0] data_in,
+    input wire [2:0] addr,
+    input wire we,
+
+    output wire [7:0] a0,
+    output wire [7:0] a1,
+    output wire [7:0] b0,
+    output wire [7:0] b1
+);
+
+reg [7:0] mem [0:7];
+
+always @(posedge clk) begin
+    if(we)
+        mem[addr] <= data_in;
+end
+
+assign a0 = mem[0];
+assign a1 = mem[1];
+assign b0 = mem[2];
+assign b1 = mem[3];
+
+endmodule
