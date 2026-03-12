a079b0db628140bc764937f64f9cf2e9dcfb8f4c
SAM-RANJITH <141648923+SAM-RANJITH@users.noreply.github.com>
2026-03-12 16:35:23 +0530

    Update pe_braun.v

--- src/pe_braun.v
+++ src/pe_braun.v
@@ -12,27 +12,27 @@ output reg [7:0] a_out,
 output reg [7:0] b_out,
 
 output reg [15:0] c_out
+
 );
 
 wire [15:0] mult;
 
 braun_multiplier mult_unit(
-    .A(a_in),
-    .B(b_in),
-    .P(mult)
+.A(a_in),
+.B(b_in),
+.P(mult)
 );
 
-always @(posedge clk) begin
+always @(posedge clk)
+begin
 
-    if(rst) begin
-        c_out <= 0;
-    end
-    else begin
-        c_out <= c_out + mult;
-    end
+if(rst)
+c_out <= 0;
+else
+c_out <= c_out + mult;
 
-    a_out <= a_in;
-    b_out <= b_in;
+a_out <= a_in;
+b_out <= b_in;
 
 end
 
