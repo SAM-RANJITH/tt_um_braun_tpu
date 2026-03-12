0b4522c9f5d7e3a0f34a8d238bd058caba1fb93b
SAM-RANJITH <141648923+SAM-RANJITH@users.noreply.github.com>
2026-03-12 16:53:31 +0530

    Update memory.v

--- src/memory.v
+++ src/memory.v
@@ -1,28 +1,37 @@
 `default_nettype none
 
 module memory(
-input wire clk,
-input wire [7:0] data_in,
-input wire [2:0] addr,
-input wire we,
+    input wire clk,
+    input wire [7:0] data_in,
+    input wire [2:0] addr,
+    input wire we,
 
-output wire [7:0] a0,
-output wire [7:0] a1,
-output wire [7:0] b0,
-output wire [7:0] b1
+    output wire [7:0] weight0,
+    output wire [7:0] weight1,
+    output wire [7:0] weight2,
+    output wire [7:0] weight3,
+
+    output wire [7:0] input0,
+    output wire [7:0] input1,
+    output wire [7:0] input2,
+    output wire [7:0] input3
 );
 
-reg [7:0] mem[0:7];
+reg [7:0] mem [0:7];
 
-always @(posedge clk)
-begin
- if(we)
-   mem[addr] <= data_in;
+always @(posedge clk) begin
+    if (we)
+        mem[addr] <= data_in;
 end
 
-assign a0 = mem[0];
-assign a1 = mem[1];
-assign b0 = mem[4];
-assign b1 = mem[5];
+assign weight0 = mem[0];
+assign weight1 = mem[1];
+assign weight2 = mem[2];
+assign weight3 = mem[3];
+
+assign input0 = mem[4];
+assign input1 = mem[5];
+assign input2 = mem[6];
+assign input3 = mem[7];
 
 endmodule
