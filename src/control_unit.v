82019786df9d6fd17fb6bdce976b63113911a095
SAM-RANJITH <141648923+SAM-RANJITH@users.noreply.github.com>
2026-03-13 11:27:04 +0530

    Update control_unit.v

--- src/control_unit.v
+++ src/control_unit.v
@@ -4,24 +4,32 @@ module control_unit(
 
 input wire clk,
 input wire rst,
-input wire start,
-output reg done
+input wire load_en,
+
+output reg [2:0] mem_addr,
+output reg mmu_en,
+output reg [2:0] mmu_cycle
 
 );
 
-reg [3:0] state;
+always @(posedge clk)
+begin
 
-always @(posedge clk) begin
+if (rst)
+begin
+    mem_addr <= 3'b000;
+    mmu_cycle <= 3'b000;
+    mmu_en <= 1'b0;
+end
 
-if(rst)
-state <= 0;
 else
-state <= state + 1;
+begin
+    if(load_en)
+        mem_addr <= mem_addr + 1;
 
-if(state == 10)
-done <= 1;
-else
-done <= 0;
+    mmu_cycle <= mmu_cycle + 1;
+    mmu_en <= 1'b1;
+end
 
 end
 
