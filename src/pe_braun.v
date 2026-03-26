9020cb36a1c3c632aefd96836f6a6789a9febf59
SAM-RANJITH <141648923+SAM-RANJITH@users.noreply.github.com>
2026-03-26 13:23:07 +0530

    Update pe_braun.v

--- src/pe_braun.v
+++ src/pe_braun.v
@@ -1,39 +1,29 @@
-`default_nettype none
-
 module pe_braun(
+    input wire clk,
+    input wire rst,
 
-input wire clk,
-input wire rst,
+    input wire [7:0] a_in,
+    input wire [7:0] b_in,
 
-input wire [7:0] a_in,
-input wire [7:0] b_in,
+    output reg [7:0] a_out,
+    output reg [7:0] b_out,
 
-output reg [7:0] a_out,
-output reg [7:0] b_out,
-
-output reg [15:0] c_out
-
+    output reg [15:0] c_out
 );
 
 wire [15:0] mult;
+assign mult = a_in * b_in;
 
-braun_multiplier mult_unit(
-.A(a_in),
-.B(b_in),
-.P(mult)
-);
+always @(posedge clk) begin
+    if (rst) begin
+        c_out <= 0;
+    end else begin
+        if (^a_in !== 1'bx && ^b_in !== 1'bx)
+            c_out <= c_out + mult;
+    end
 
-always @(posedge clk)
-begin
-
-if(rst)
-c_out <= 0;
-else
-c_out <= c_out + mult;
-
-a_out <= a_in;
-b_out <= b_in;
-
+    a_out <= a_in;
+    b_out <= b_in;
 end
 
 endmodule
