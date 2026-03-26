d99e9e06094ec48a15b299c1dee08a77d4c7522b
SAM-RANJITH <141648923+SAM-RANJITH@users.noreply.github.com>
2026-03-26 13:25:17 +0530

    Update control_unit.v

--- src/control_unit.v
+++ src/control_unit.v
@@ -1,36 +1,34 @@
-`default_nettype none
-
 module control_unit(
+    input wire clk,
+    input wire rst,
+    input wire load_en,
 
-input wire clk,
-input wire rst,
-input wire load_en,
-
-output reg [2:0] mem_addr,
-output reg mmu_en,
-output reg [2:0] mmu_cycle
-
+    output reg [2:0] mem_addr,
+    output reg mmu_en,
+    output reg [2:0] mmu_cycle
 );
 
-always @(posedge clk)
-begin
+reg [3:0] load_count;
 
-if (rst)
-begin
-    mem_addr <= 3'b000;
-    mmu_cycle <= 3'b000;
-    mmu_en <= 1'b0;
-end
+always @(posedge clk) begin
+    if (rst) begin
+        mem_addr <= 0;
+        mmu_cycle <= 0;
+        mmu_en <= 0;
+        load_count <= 0;
+    end else begin
 
-else
-begin
-    if(load_en)
-        mem_addr <= mem_addr + 1;
+        if (load_en) begin
+            mem_addr <= mem_addr + 1;
+            load_count <= load_count + 1;
+        end
 
-    mmu_cycle <= mmu_cycle + 1;
-    mmu_en <= 1'b1;
-end
-
+        // Start compute AFTER loading 8 values
+        if (load_count == 8) begin
+            mmu_en <= 1;
+            mmu_cycle <= mmu_cycle + 1;
+        end
+    end
 end
 
 endmodule
