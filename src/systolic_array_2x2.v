3041f473a9e8d7dab44567d859a371114e64228e
SAM-RANJITH <141648923+SAM-RANJITH@users.noreply.github.com>
2026-03-26 13:33:45 +0530

    Update systolic_array_2x2.v

--- src/systolic_array_2x2.v
+++ src/systolic_array_2x2.v
@@ -1,6 +1,7 @@
 module systolic_array_2x2(
     input wire clk,
     input wire rst,
+    input wire ena,
 
     input wire [7:0] a0,
     input wire [7:0] a1,
@@ -16,9 +17,9 @@ module systolic_array_2x2(
 wire [7:0] a00,a01,a10,a11;
 wire [7:0] b00,b01,b10,b11;
 
-pe_braun pe00(clk,rst,a0,b0,a00,b00,c00);
-pe_braun pe01(clk,rst,a00,b1,a01,b01,c01);
-pe_braun pe10(clk,rst,a1,b00,a10,b10,c10);
-pe_braun pe11(clk,rst,a10,b01,a11,b11,c11);
+pe_braun pe00(clk,rst,ena,a0,b0,a00,b00,c00);
+pe_braun pe01(clk,rst,ena,a00,b1,a01,b01,c01);
+pe_braun pe10(clk,rst,ena,a1,b00,a10,b10,c10);
+pe_braun pe11(clk,rst,ena,a10,b01,a11,b11,c11);
 
 endmodule
