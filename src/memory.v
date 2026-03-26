a20b672912fa4215a57b349cc858897a37605f54
SAM-RANJITH <141648923+SAM-RANJITH@users.noreply.github.com>
2026-03-26 13:34:13 +0530

    Update memory.v

--- src/memory.v
+++ src/memory.v
@@ -1,5 +1,6 @@
 module memory(
     input wire clk,
+    input wire ena,
     input wire [7:0] data_in,
     input wire [2:0] addr,
     input wire we,
@@ -15,20 +16,20 @@ module memory(
     output wire [7:0] input3
 );
 
-reg [7:0] mem [0:7];
+reg [7:0] mem_array [0:7];
 
 always @(posedge clk)
-    if (we)
-        mem[addr] <= data_in;
+    if (ena && we)
+        mem_array[addr] <= data_in;
 
-assign weight0 = mem[0];
-assign weight1 = mem[1];
-assign weight2 = mem[2];
-assign weight3 = mem[3];
+assign weight0 = mem_array[0];
+assign weight1 = mem_array[1];
+assign weight2 = mem_array[2];
+assign weight3 = mem_array[3];
 
-assign input0 = mem[4];
-assign input1 = mem[5];
-assign input2 = mem[6];
-assign input3 = mem[7];
+assign input0 = mem_array[4];
+assign input1 = mem_array[5];
+assign input2 = mem_array[6];
+assign input3 = mem_array[7];
 
 endmodule
