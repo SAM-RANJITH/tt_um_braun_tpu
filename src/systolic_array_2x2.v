cf813a9f36fcc59206b228f1ba545f2e655b2b3c
SAM-RANJITH <141648923+SAM-RANJITH@users.noreply.github.com>
2026-03-26 13:49:04 +0530

    Update systolic_array_2x2.v

--- src/systolic_array_2x2.v
+++ src/systolic_array_2x2.v
@@ -1,25 +1,15 @@
 module systolic_array_2x2(
-    input wire clk,
-    input wire rst,
-    input wire ena,
-
-    input wire [7:0] a0,
-    input wire [7:0] a1,
-    input wire [7:0] b0,
-    input wire [7:0] b1,
-
-    output wire [15:0] c00,
-    output wire [15:0] c01,
-    output wire [15:0] c10,
-    output wire [15:0] c11
+    input clk,rst,ena,
+    input [7:0] a0,a1,b0,b1,
+    output [15:0] c00,c01,c10,c11
 );
 
 wire [7:0] a00,a01,a10,a11;
 wire [7:0] b00,b01,b10,b11;
 
-pe_braun pe00(clk,rst,ena,a0,b0,a00,b00,c00);
-pe_braun pe01(clk,rst,ena,a00,b1,a01,b01,c01);
-pe_braun pe10(clk,rst,ena,a1,b00,a10,b10,c10);
-pe_braun pe11(clk,rst,ena,a10,b01,a11,b11,c11);
+pe p0(clk,rst,ena,a0,b0,a00,b00,c00);
+pe p1(clk,rst,ena,a00,b1,a01,b01,c01);
+pe p2(clk,rst,ena,a1,b00,a10,b10,c10);
+pe p3(clk,rst,ena,a10,b01,a11,b11,c11);
 
 endmodule
