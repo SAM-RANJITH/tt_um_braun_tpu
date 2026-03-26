2934b72c0b0afd18e6fdbc45ec8dc0f9db49a232
SAM-RANJITH <141648923+SAM-RANJITH@users.noreply.github.com>
2026-03-26 13:48:42 +0530

    Update memory.v

--- src/memory.v
+++ src/memory.v
@@ -1,35 +1,31 @@
 module memory(
-    input wire clk,
-    input wire ena,
-    input wire [7:0] data_in,
-    input wire [2:0] addr,
-    input wire we,
-
-    output wire [7:0] weight0,
-    output wire [7:0] weight1,
-    output wire [7:0] weight2,
-    output wire [7:0] weight3,
-
-    output wire [7:0] input0,
-    output wire [7:0] input1,
-    output wire [7:0] input2,
-    output wire [7:0] input3
+    input clk, ena, we,
+    input [7:0] data_in,
+    input [2:0] addr,
+    output [7:0] w0,w1,w2,w3,
+    output [7:0] i0,i1,i2,i3
 );
 
 reg [7:0] mem_array [0:7];
+integer k;
+
+initial begin
+    for (k=0;k<8;k=k+1)
+        mem_array[k]=0;
+end
 
 always @(posedge clk)
     if (ena && we)
         mem_array[addr] <= data_in;
 
-assign weight0 = mem_array[0];
-assign weight1 = mem_array[1];
-assign weight2 = mem_array[2];
-assign weight3 = mem_array[3];
+assign w0=mem_array[0];
+assign w1=mem_array[1];
+assign w2=mem_array[2];
+assign w3=mem_array[3];
 
-assign input0 = mem_array[4];
-assign input1 = mem_array[5];
-assign input2 = mem_array[6];
-assign input3 = mem_array[7];
+assign i0=mem_array[4];
+assign i1=mem_array[5];
+assign i2=mem_array[6];
+assign i3=mem_array[7];
 
 endmodule
