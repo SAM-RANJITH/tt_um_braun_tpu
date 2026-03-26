03abfa5447ad9b1a9d53b06b2971626ea6f057c9
SAM-RANJITH <141648923+SAM-RANJITH@users.noreply.github.com>
2026-03-26 13:33:28 +0530

    Update pe_braun.v

--- src/pe_braun.v
+++ src/pe_braun.v
@@ -1,6 +1,7 @@
 module pe_braun(
     input wire clk,
     input wire rst,
+    input wire ena,
 
     input wire [7:0] a_in,
     input wire [7:0] b_in,
@@ -11,19 +12,16 @@ module pe_braun(
     output reg [15:0] c_out
 );
 
-wire [15:0] mult;
-assign mult = a_in * b_in;
+wire [15:0] mult = a_in * b_in;
 
 always @(posedge clk) begin
     if (rst) begin
         c_out <= 0;
-    end else begin
-        if (^a_in !== 1'bx && ^b_in !== 1'bx)
-            c_out <= c_out + mult;
+    end else if (ena) begin
+        c_out <= c_out + mult;
+        a_out <= a_in;
+        b_out <= b_in;
     end
-
-    a_out <= a_in;
-    b_out <= b_in;
 end
 
 endmodule
